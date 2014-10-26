445b3b6cd10a0743bb9ac40c071f15ee2976bf82
Theodor Vararu <theo@vararu.org>
2014-10-26 18:28:07 +0000

    Uncomment everything!

--- install.sh
+++ install.sh
@@ -2,14 +2,14 @@
 
 echo "\n Hey there. Never mind me, just setting up your system.\n"
 
-#./defaults/osx.sh
+./defaults/osx.sh
 
-#./homebrew/install.sh
+./homebrew/install.sh
 
 echo " Installing oh-my-zsh. (https://github.com/robbyrussell/oh-my-zsh)"
-#curl -L http://install.ohmyz.sh | sh
-#cp -f .zshrc ~
-#cp -rf .oh-my-zsh-custom ~
+curl -L http://install.ohmyz.sh | sh
+cp -f .zshrc ~
+cp -rf .oh-my-zsh-custom ~
 echo "zsh configured.\n"
 
 echo " All done! Enjoy.\n"
