f574bf1b904e6ea82059bb67868e55e9710dd1de
Theodor Vararu <theo@vararu.org>
2015-09-23 21:37:41 +0100

    Update brew cask installs.

--- homebrew/brew-cask-install.sh
+++ homebrew/brew-cask-install.sh
@@ -7,32 +7,13 @@ brew cask install alfred
 brew cask install atom
 brew cask install bettertouchtool
 brew cask install dropbox
-brew cask install firefox
-brew cask install flash
 brew cask install flux
 brew cask install iterm2
-brew cask install google-chrome
-brew cask install knock
-brew cask install macvim
 brew cask install mailbox
-brew cask install radiant-player
-# brew cask install safaritabswitching
+brew cask install nosleep
+brew cask install screenhero
 brew cask install slack
 brew cask install spectacle
-brew cask install telegram
 brew cask install transmission
-brew cask install xquartz
-
-echo "Running brew cask caveats."
-
-# Link brew cask installed apps into Alfred.
-brew cask alfred
-
-# Safaritabswitching isn't updated, boo.
-mkdir -p ~/.tmp/
-wget https://github.com/rs/SafariTabSwitching/releases/download/1.2.7/Safari.Tab.Switching-1.2.7.zip -P ~/.tmp/
-unzip ~/.tmp/Safari.Tab.Switching-1.2.7.zip
-open ~/.tmp/Safari\ Tab\ Switching-1.2.7.pkg
 
 echo "Successfully installed all brew cask apps.\n"
-
