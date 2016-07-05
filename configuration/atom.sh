7a73440d375dc5776eb9dcb10beada89553fc5b7
Theodor Vararu <theodor.vararu@digital.cabinet-office.gov.uk>
2016-07-05 11:08:04 +0100

    Update atom community packages

--- configuration/atom.sh
+++ configuration/atom.sh
@@ -2,14 +2,15 @@
 
 echo " Installing atom packages."
 
+apm install atom-beautify
 apm install autocomplete-paths
 apm install case-switch
 apm install editorconfig
 apm install ex-mode
 apm install file-icons
 apm install linter
-apm install linter-coffeelint
 apm install linter-js-standard
+apm install pretty-json
 apm install react
 apm install sort-lines
 apm install standard-formatter
