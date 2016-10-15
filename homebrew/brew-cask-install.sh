0fed0c63f8d1502fbef4cbe92a08f7e029515245
Theodor Vararu <theo@vararu.org>
2016-10-15 09:57:05 +0100

    Add qlmarkdown and 1password to brew-cask-install.sh

--- homebrew/brew-cask-install.sh
+++ homebrew/brew-cask-install.sh
@@ -1,11 +1,13 @@
 echo " Installing native apps using \`brew cask\`."
 
+brew cask install 1password
 brew cask install alfred
 brew cask install atom
 brew cask install dropbox
 brew cask install flux
 brew cask install iterm2
 brew cask install nosleep
+brew cask install qlmarkdown
 brew cask install slack
 brew cask install spectacle
 brew cask install transmission
