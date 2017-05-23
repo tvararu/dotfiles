cfccb6dc0df5b83aceaed7d1aca334130d6a458b
Theodor Vararu <theo@vararu.org>
2017-05-23 23:18:07 +0100

    Update brew cask

--- homebrew/brew-cask-install.sh
+++ homebrew/brew-cask-install.sh
@@ -5,14 +5,12 @@ echo " Installing native apps using \`brew cask\`."
 brew cask install 1password
 brew cask install alfred
 brew cask install atom
-brew cask install dropbox
-brew cask install flux
 brew cask install iterm2
 brew cask install keybase
 brew cask install nosleep
 brew cask install qlmarkdown
-brew cask install slack
 brew cask install spectacle
 brew cask install transmission
+brew cask install vlc
 
 echo " Successfully installed all brew cask apps.\n"
