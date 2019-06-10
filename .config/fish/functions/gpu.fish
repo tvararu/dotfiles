e3e6fc64f776a182cd94a2edddf71220e8a14716
Theodor Vararu <theo@vararu.org>
2019-06-10 11:29:59 +0100

    Remove prevention part of gpu
    This can be done more elegantly using branch protection.

--- .config/fish/functions/gpu.fish
+++ .config/fish/functions/gpu.fish
@@ -1,8 +1,3 @@
 function gpu
-	set branch_name (git rev-parse --abbrev-ref HEAD)
-if [ $branch_name = "master" ]
-  echo "Don't gpu directly to master."
-else
-  git push -u origin $branch_name
-end
+	git push -u origin $branch_name
 end
