2b9bd5bc54bf0911a598bc15f2515b62b8613b44
Theodor Vararu <theo@vararu.org>
2019-06-10 20:11:14 +0100

    Run backup-dotfiles

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,5 @@
 # Remove the newcomer fish greeting.
 set fish_greeting ""
+set -x EDITOR vim
+
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
--- .vimrc
+++ .vimrc
@@ -7,3 +7,4 @@ set shiftwidth=2
 set softtabstop=2
 set expandtab
 set textwidth=70
+set autoindent
