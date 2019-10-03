3b83edf348c790166e8fdcddb88f5d75f631dca9
Theodor Vararu <theo@vararu.org>
2019-10-03 22:39:25 +0100

    Add more atom packages

--- configuration/atom.sh
+++ configuration/atom.sh
@@ -2,14 +2,21 @@
 
 echo " Installing atom packages."
 
+
 apm install atom-beautify
 apm install autocomplete-paths
 apm install case-switch
 apm install editorconfig
+apm install ex-mode
 apm install file-icons
+apm install intentions
 apm install language-babel
+apm install linter
+apm install linter-ui-default
+apm install no-title-bar
 apm install pigments
 apm install prettier-atom
+apm install single-click-open
 apm install sort-lines
 apm install toggle-quotes
 apm install vim-mode-plus
