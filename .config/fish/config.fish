50492be433c1a21f51ab12c69078ade32cc22996
Theodor Vararu <theo@vararu.org>
2019-10-20 10:47:57 +0100

    Update fish config, remove sls tabtab

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,12 +4,4 @@ set -x EDITOR vim
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
-# tabtab source for serverless package
-# uninstall by removing these lines or running `tabtab uninstall serverless`
-[ -f /Users/tvararu/.config/yarn/global/node_modules/tabtab/.completions/serverless.fish ]; and . /Users/tvararu/.config/yarn/global/node_modules/tabtab/.completions/serverless.fish
-# tabtab source for sls package
-# uninstall by removing these lines or running `tabtab uninstall sls`
-[ -f /Users/tvararu/.config/yarn/global/node_modules/tabtab/.completions/sls.fish ]; and . /Users/tvararu/.config/yarn/global/node_modules/tabtab/.completions/sls.fish
-# tabtab source for slss package
-# uninstall by removing these lines or running `tabtab uninstall slss`
-[ -f /Users/tvararu/.config/yarn/global/node_modules/tabtab/.completions/slss.fish ]; and . /Users/tvararu/.config/yarn/global/node_modules/tabtab/.completions/slss.fish
+status --is-interactive; and source (rbenv init -|psub)
