a6589e5d555ba1a42e61f6394e0054a2ade050bc
Theodor Vararu <theo@vararu.org>
2020-09-24 12:12:44 +0100

    Remove iterm2 integration and rbenv from config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,3 @@
 # Remove the newcomer fish greeting.
 set fish_greeting ""
 set -x EDITOR vim
-
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
-
-status --is-interactive; and source (rbenv init -|psub)
